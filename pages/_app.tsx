2f54353bfb2f708c5d736307102a3f5c3d731f78
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2020-07-30 22:43:10 +0100

    feat: add favicon and change title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }: Props): React.ReactElement {
   return (
     <ThemeProvider>
       <Head>
-        <title>guilherme.codes</title>
+        <title>guilherme de andrade | software engineer</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
